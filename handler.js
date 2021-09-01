cf2371881736dbfefffaacdd2eadfb0e263ca872
duslerke <liudvikast@gmail.com>
2021-09-01 11:20:24 +0100

    Defined API resource endpoint's needed.

--- handler.js
+++ handler.js
@@ -1,18 +1,21 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports = {
+  hello: async (event) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: "Go Serverless v1.0! Your function executed successfully!",
+          input: event,
+        },
+        null,
+        2
+      ),
+    };
+  },
+  createAPI: async (event, context) => {},
+  listAPIs: async (event, context) => {},
+  getAPI: async (event, context) => {},
+  patchAPI: async (event, context) => {},
 };
