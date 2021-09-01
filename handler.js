4717315919f8473a37060c5edb0c30d2b9752169
duslerke <liudvikast@gmail.com>
2021-09-01 12:53:48 +0100

    Basic createAPI endpoint validation, and not implemented response..

--- handler.js
+++ handler.js
@@ -14,6 +14,32 @@ module.exports = {
       ),
     };
   },
+  createAPI: async (event, context) => {
+    let payload = {};
+    try {
+      payload = JSON.parse(event.body);
+    } catch (jsonError) {
+      console.log("There was an error parsing json body:\n", jsonError);
+      return {
+        statusCode: 400,
+      };
+    }
+
+    // basic validation
+    if (
+      typeof payload.name === "undefined" ||
+      typeof payload.baseUrl === "undefined"
+    ) {
+      console.log("Missing required parameters");
+      return {
+        statusCode: 422,
+      };
+    }
+
+    return {
+      statusCode: 501,
+    };
+  },
   createAPI: async (event, context) => {},
   listAPIs: async (event, context) => {},
   getAPI: async (event, context) => {},
