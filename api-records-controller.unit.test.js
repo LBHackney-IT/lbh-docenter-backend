262fc6b8d79b5a7e82157b8fecbcec484de44787
duslerke <liudvikast@gmail.com>
2021-09-04 10:53:24 +0100

    Configured passing environment unit testing dedicated variables to unit tests.

--- api-records-controller.unit.test.js
+++ api-records-controller.unit.test.js
@@ -1,5 +1,11 @@
+const { createAPI } = require("./api-records-controller");
+
 describe("API Records Controller", () => {
-  it("math is not broken", () => {
-    expect(1).toEqual(1);
+  describe("CreateAPI function", () => {
+    it("should call the presentationToDomain mapper method once", () => {
+      expect(process.env.JEST_UNIT).toEqual("1");
+      expect(process.env.DYNAMODB_APIS_TABLE).toEqual("apis");
+      expect(process.env.PUKA).toEqual("meska");
+    });
   });
 });
