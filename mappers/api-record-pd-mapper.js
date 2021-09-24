cce3ebff25bc3800e4e506d614d773ea254480a7
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2021-09-24 22:30:37 +0100

    Stop the nested data access flow when parent is null - nested objects are not guaranteed to non-empty.

--- mappers/api-record-pd-mapper.js
+++ mappers/api-record-pd-mapper.js
@@ -28,23 +28,23 @@ class PresentationDomainMapper {
   // sub-mappers
   toDomain_OtherDocumentation(userInput) {
     return new OtherDocumentation({
-      businessContext: userInput.businessContext,
-      dataModel: userInput.dataModel,
+      businessContext: userInput?.businessContext,
+      dataModel: userInput?.dataModel,
     });
   }
 
   toDomain_Endpoint(userInput) {
     return new Endpoint({
-      httpMethod: userInput.httpMethod,
-      name: userInput.name,
+      httpMethod: userInput?.httpMethod,
+      name: userInput?.name,
     });
   }
 
   toDomain_DependencyAPI(userInput) {
     return new DependencyAPI({
-      apiId: userInput.apiId,
-      apiName: userInput.apiName,
-      endpointsUsingIt: userInput.endpointsUsingIt.map((endpoint) =>
+      apiId: userInput?.apiId,
+      apiName: userInput?.apiName,
+      endpointsUsingIt: userInput?.endpointsUsingIt?.map((endpoint) =>
         this.toDomain_Endpoint(endpoint)
       ),
     });
@@ -52,35 +52,35 @@ class PresentationDomainMapper {
 
   toDomain_DependencyScript(userInput) {
     return new DependencyScript({
-      name: userInput.name,
-      description: userInput.description,
+      name: userInput?.name,
+      description: userInput?.description,
     });
   }
 
   toDomain_DependencyDatabase(userInput) {
     return new DependencyDatabase({
-      name: userInput.name,
-      technicalName: userInput.technicalName,
-      type: userInput.type,
-      hostedAt: userInput.hostedAt,
+      name: userInput?.name,
+      technicalName: userInput?.technicalName,
+      type: userInput?.type,
+      hostedAt: userInput?.hostedAt,
     });
   }
 
   toDomain_Environments(userInput) {
     return new Environments({
-      development: userInput.development,
-      staging: userInput.staging,
-      production: userInput.production,
+      development: userInput?.development,
+      staging: userInput?.staging,
+      production: userInput?.production,
     });
   }
 
   toDomain_Dependencies(userInput) {
     return new Dependencies({
-      apis: userInput.apis.map((api) => this.toDomain_DependencyAPI(api)),
-      scripts: userInput.scripts.map((script) =>
+      apis: userInput?.apis?.map((api) => this.toDomain_DependencyAPI(api)),
+      scripts: userInput?.scripts?.map((script) =>
         this.toDomain_DependencyScript(script)
       ),
-      databases: userInput.databases.map((database) =>
+      databases: userInput?.databases?.map((database) =>
         this.toDomain_DependencyDatabase(database)
       ),
     });
